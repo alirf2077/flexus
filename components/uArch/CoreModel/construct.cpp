3c488afebc4e509aa116a3350f919a0113c59bd0
Alireza Foroodnia <foroodni@iccluster113.iccluster.epfl.ch>
2025-12-05 17:12:33 +0000

    removed hand coded inOrderExecute

--- components/uArch/CoreModel/construct.cpp
+++ components/uArch/CoreModel/construct.cpp
@@ -93,7 +93,7 @@ CoreImpl::CoreImpl(uArchOptions_t options,
   theTrackParallelAccesses(options.trackParallelAccesses)
   , /* CMU-ONLY */
   theInOrderMemory(options.inOrderMemory)
-  , theInOrderExecute(true)
+  , theInOrderExecute(options.inOrderExecute)
   , theIdleThisCycle(false)
   , theIdleCycleCount(0)
   , theBBVTracker(/*BBVTracker::createBBVTracker(aNode)*/ 0)
